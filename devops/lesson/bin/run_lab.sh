477217a1ed4cf5ac4b28e835b722bde8526b34e6
Guillaume QUINQUET <guillaume.quinquet@hotmail.fr>
2023-02-15 16:48:04 +0100

    Updated file structure, dockerfile and makefile to implement infra easy setup

---
+++ devops/lesson/bin/run_lab.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+SCRIPTPATH=$(builtin cd $SCRIPTPATH/../../mlflow_server; pwd)
+
+docker run -it --rm --user root -p 10000:8888 -p 8000:8000 -p 4200:4200 -p 5002:5002 -v ${SCRIPTPATH}/local:/mlflow -e JUPYTER_ENABLE_LAB=yes --network mlops-crashcourse-supinfo --name jupyter -d mlops_notebooks_supinfo
